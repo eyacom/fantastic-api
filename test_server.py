47b828893791cd6964cadba939541cac4cb4c7c7
Lina-Hammami <hamemilina@gmail.com>
2020-06-13 15:54:05 +0100

    Update file test_server.py : Fix typo Code linted

--- test_server.py
+++ test_server.py
@@ -1,6 +1,7 @@
 from server import TodoDAO
 import pytest
 
+
 def test_list_todo():
     DAO = TodoDAO()
     t1 = DAO.create({'task': 'test-todo'})
@@ -9,30 +10,34 @@ def test_list_todo():
     t2 = DAO.get(t1['id'])
     assert t2 is not None
     assert t2['task'] == t1['task']
-    assert t2['createdAt'] is not None 
+    assert t2['createdAt'] is not None
+
 
 def test_empty_todo():
     DAO = TodoDAO()
     assert len(DAO.todos) == 0
 
 
-class TaskNotFoundException(Exception): 
-    pass   
+class TaskNotFoundException(Exception):
+    pass
+
 
 def empty_task():
     DAO = TodoDAO()
     t = DAO.create({'task': ''})
-    #If the task is empty
-    if not t['task'] :
+    # If the task is empty
+    if not t['task']:
         raise TaskNotFoundException("Exception TaskNotFound raised")
+
 
 def none_task():
     DAO = TodoDAO()
     t2 = DAO.create({'task': None})
+    # If the task is null
     if t2['task'] is None:
         raise AssertionError("Invalid null task!")
 
-# TODO : add test raise exception when task is absent -->  ToCheck 
+
 def test_notfound_todo():
     with pytest.raises(TaskNotFoundException):
         empty_task()
