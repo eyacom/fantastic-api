9460cf2c1dfa618b37f6b9614d35d6b0972b3b93
OlfaBensoussia <olfa.bensoussia@ensi-uma.tn>
2020-06-18 19:47:05 +0200

    suggest a solution to resolve issue-1

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_restx import Api, Resource, fields
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 
 # THIS CODE IS DERIVATED FROM THE EXAMPLE OF Flask-RESTX EXTENSION
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
 api = Api(
     app,
     version='1.0',
@@ -14,66 +19,67 @@ api = Api(
 
 ns = api.namespace('todos', description='TODO operations')
 
-todo = api.model(
-    'Todo', {
-        'id':
-        fields.Integer(readOnly=True,
-                       description='The task unique identifier'),
-        'createdAt':
-        fields.DateTime(readOnly=True,
-                        description='datetime value of task creation time'),
-        'task':
-        fields.String(required=True, description='The task details')
-    })
+
+class TodoDB(db.Model):
+    id_t = db.Column('id_t', db.Integer, primary_key=True)
+    task = db.Column('task', db.String(300))
 
 
 class TodoDAO(object):
-    def __init__(self):
-        self.counter = 0
-        self.todos = []
-
     def get(self, id):
-        for todo in self.todos:
-            # TODO : Improve the searching complexity to O(1) using hashmap structure
-            if todo['id'] == id:
-                return todo
+        todo = TodoDB.query.filter_by(id_t=id)
+        if todo is not None:
+            return todo._asdict()
         api.abort(404, "Todo {} doesn't exist".format(id))
 
-    def create(self, data):
-        todo = data
-        todo['id'] = self.counter = self.counter + 1
-        todo['createdAt'] = datetime.now()
-        self.todos.append(todo)
+# this method will be used to print all elements in a dictionary format
+
+    def print_all(self):
+        todo = TodoDB.query.all()
+# We used a for loop because _asdict() works on a row
+        for u in todo:
+            print(u._asdict())
         return todo
 
+#  this method will be used to get all elements
+
+    def get_all(self):
+        todo = TodoDB.query.all()
+        return todo
+
+    def create(self, data):
+        todo_task = data['task']
+        todo_id = TodoDB.query.count() + 1
+        todo = TodoDB(todo_id, todo_task)
+        db.session.add(todo)
+        db.session.commit()
+        return todo._asdict()
+
     def update(self, id, data):
-        todo = self.get(id)
-        todo.update(data)
+        todo = TodoDB.query.filter_by(id_t=id)
+        todo.task = data['task']
+        db.session.commit()
         return todo
 
     def delete(self, id):
-        todo = self.get(id)
-        self.todos.remove(todo)
+        todo = TodoDB.query.filter_by(id_t=id)
+        db.session.delete(todo)
+        db.session.commit()
 
 
 DAO = TodoDAO()
-DAO.create({'task': 'Build an API'})
-DAO.create({'task': '?????'})
-DAO.create({'task': 'profit!'})
 
 
 @ns.route('/')
 class TodoList(Resource):
     '''Shows a list of all todos, and lets you POST to add new tasks'''
     @ns.doc('list_todos')
-    @ns.marshal_list_with(todo)
+    # @ns.marshal_list_with(todo)
     def get(self):
         '''List all tasks'''
-        return DAO.todos
+        return DAO.print_all()
 
     @ns.doc('create_todo')
-    @ns.expect(todo)
-    @ns.marshal_with(todo, code=201)
     def post(self):
         '''Create a new task'''
         return DAO.create(api.payload), 201
@@ -85,7 +91,7 @@ class TodoList(Resource):
 class Todo(Resource):
     '''Show a single todo item and lets you delete them'''
     @ns.doc('get_todo')
-    @ns.marshal_with(todo)
+    # @ns.marshal_with(todo)
     def get(self, id):
         '''Fetch a given resource'''
         return DAO.get(id)
@@ -97,8 +103,6 @@ class Todo(Resource):
         DAO.delete(id)
         return '', 204
 
-    @ns.expect(todo)
-    @ns.marshal_with(todo)
     def put(self, id):
         '''Update a task given its identifier'''
         return DAO.update(id, api.payload)
